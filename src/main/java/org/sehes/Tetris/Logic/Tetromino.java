d519151c3fd88052bbeb51edc46a48c7e0d15a30
Jan Kocábek <166728580+JanKocabek@users.noreply.github.com>
2025-02-28 21:07:09 +0100

    hiding coordnation as result of curent grid position instead of direct variables

--- src/main/java/org/sehes/Tetris/Logic/Tetromino.java
+++ src/main/java/org/sehes/Tetris/Logic/Tetromino.java
@@ -7,13 +7,10 @@ import java.util.Random;
 
 public class Tetromino {
     private final int SIZEREC = 30;
-    // private final Rectangle2D.Double rectangle;
     private final Color color;
     private final boolean[][] grid;
     private final int[] position;
     private final int[] STARTPOS = {4, 0};/*X left to right, Y up to bottom*/
-    private int xCoord;
-    private int yCoord;
     private final static Random random = new Random();
 
     public static Tetromino tetrominoFactory() {
@@ -25,8 +22,6 @@ public class Tetromino {
         color = type.getColor();
         grid = type.getGrid();
         position = STARTPOS;
-        xCoord = position[0] * SIZEREC;
-        yCoord = (position[1] - 1) * SIZEREC;
     }
 
     public Color getColor() {
@@ -40,16 +35,14 @@ public class Tetromino {
     public void move(DirectionFlag flag) {
         position[0] += flag.getX();
         position[1] += flag.getY();
-        xCoord = position[0] * SIZEREC;
-        yCoord = (position[1] - 1) * SIZEREC;
     }
 
     public int getxCoord() {
-        return xCoord;
+        return  position[0] * SIZEREC;
     }
 
     public int getyCoord() {
-        return yCoord;
+        return (position[1] - 1) * SIZEREC;
     }
 
     public boolean[][] getGrid() {
@@ -60,74 +53,67 @@ public class Tetromino {
         return position;
     }
 
+    enum TETROMINO_TYPE {
 
-//    public Rectangle2D.Double getRectangle() {
-//        return rectangle;
-//    }
-}
+        I(new boolean[][]{
+                {true, true, true, true}}, Color.CYAN, 0),
+        J(new boolean[][]{
+                {true, false, false},
+                {true, true, true}}, Color.BLUE, 1),
+        L(new boolean[][]{
+                {false, false, true},
+                {true, true, true}}, Color.ORANGE, 2),
 
-enum TETROMINO_TYPE {
+        O(new boolean[][]{
+                {true, true},
+                {true, true}}, Color.YELLOW, 3),
 
-    I(new boolean[][]{
-            {true, true, true, true}}, Color.CYAN, 0),
-    J(new boolean[][]{
-            {true, false, false},
-            {true, true, true}}, Color.BLUE, 1),
-    L(new boolean[][]{
-            {false, false, true},
-            {true, true, true}}, Color.ORANGE, 2),
+        S(new boolean[][]{
+                {false, true, true},
+                {true, true, false}}, Color.GREEN, 4),
 
-    O(new boolean[][]{
-            {true, true},
-            {true, true}}, Color.YELLOW, 3),
+        T(new boolean[][]{
+                {false, true, false},
+                {true, true, true}
+        }, Color.MAGENTA, 5),
 
-    S(new boolean[][]{
-            {false, true, true},
-            {true, true, false}}, Color.GREEN, 4),
+        Z(new boolean[][]{
+                {true, true, false},
+                {false, true, true}
+        }, Color.RED, 6);
 
-    T(new boolean[][]{
-            {false, true, false},
-            {true, true, true}
-    }, Color.MAGENTA, 5),
+        private final Color color;
+        private final boolean[][] grid;
+        private final int intValue;
 
-    Z(new boolean[][]{
-            {true, true, false},
-            {false, true, true}
-    }, Color.RED, 6);
+        TETROMINO_TYPE(boolean[][] grid, Color color, int intValue) {
+            this.color = color;
+            this.grid = grid;
+            this.intValue = intValue;
+        }
 
-    private final Color color;
-    private final boolean[][] grid;
-    private final int intValue;
+        public Color getColor() {
+            return color;
+        }
 
-    TETROMINO_TYPE(boolean[][] grid, Color color, int intValue) {
-        this.color = color;
-        this.grid = grid;
-        this.intValue = intValue;
-    }
+        public boolean[][] getGrid() {
+            return grid;
+        }
 
-    public Color getColor() {
-        return color;
-    }
+        public int getIntValue() {
+            return intValue;
+        }
 
-    public boolean[][] getGrid() {
-        return grid;
-    }
+        private static final Map<Integer, TETROMINO_TYPE> map = new HashMap<>();
 
-    public int getIntValue() {
-        return intValue;
-    }
+        static {
+            for (TETROMINO_TYPE type : TETROMINO_TYPE.values()) {
+                map.put(type.intValue, type);
+            }
+        }
 
-    private static final Map<Integer, TETROMINO_TYPE> map = new HashMap<>();
-
-    static {
-        for (TETROMINO_TYPE type : TETROMINO_TYPE.values()) {
-            map.put(type.intValue, type);
+        public static TETROMINO_TYPE get(int intValue) {
+            return map.get(intValue);
         }
     }
-
-    public static TETROMINO_TYPE get(int intValue) {
-        return map.get(intValue);
-    }
-
-
 }
