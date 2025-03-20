797bc8ad71e3cedcacdb2b21bec70bd93f2f66f5
Jan Kocábek <166728580+JanKocabek@users.noreply.github.com>
2025-03-20 17:06:13 +0100

    creating util classes for manipulation with matrix(aka for rotation of tetrominos )

---
+++ src/main/java/org/sehes/Tetris/Logic/util/Util.java
@@ -0,0 +1,28 @@
+package org.sehes.Tetris.Logic.util;
+
+public class Util {
+    public static boolean[][] transposeMatrix(boolean[][] matrix) {
+        if (matrix.length == 0) return matrix;
+        boolean[][] newMatrix = new boolean[matrix[0].length][matrix.length];
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[0].length; col++) {
+                newMatrix[col][row] = matrix[row][col];
+            }
+        }
+        return newMatrix;
+    }
+
+    public static void swapColumns(boolean[][] grid) {
+        if (grid.length == 0) return;
+        int startCol = 0;
+        int endCol = grid[0].length - 1 - startCol;
+        for (; startCol < endCol; startCol++) {
+            for (int row = 0; row < grid.length; row++) {
+                boolean tmp = grid[row][startCol];
+                grid[row][startCol] = grid[row][endCol];
+                grid[row][endCol] = tmp;
+            }
+            endCol--;
+        }
+    }
+}
