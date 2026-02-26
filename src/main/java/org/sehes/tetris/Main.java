e737c3fd0e304b035eecef1bb7d13eb938c50cb9
Jan Kocábek <166728580+JanKocabek@users.noreply.github.com>
2026-02-26 19:30:12 +0100

    running game from constructor

--- src/main/java/org/sehes/tetris/Main.java
+++ src/main/java/org/sehes/tetris/Main.java
@@ -11,9 +11,7 @@ public class Main {
  */
 
     public static void main(String[] args) {
-        GameManager gameManager = new GameManager();
-        gameManager.startApp();
-        
+       new GameManager();
     }
 
 }
