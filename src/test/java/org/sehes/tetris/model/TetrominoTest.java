d5a50fec64973e9f0645159a950f193a051b1e76
Jan Kocábek <166728580+JanKocabek@users.noreply.github.com>
2026-03-06 13:11:53 +0100

    basic  for futre Test of Tetromino class

--- src/test/java/org/sehes/tetris/model/TetrominoTest.java
+++ src/test/java/org/sehes/tetris/model/TetrominoTest.java
@@ -1,48 +1,47 @@
 package org.sehes.tetris.model;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.sehes.tetris.config.GameParameters;
+
 class TetrominoTest {
 
-    @BeforeEach
-    void setUp() {
+    @Test
+    void testTetrominoFactory() {
+        // given
+        Tetromino tetromino = Tetromino.tetrominoFactory(GameParameters.SPAWN_POINT);
+        // then
+        assertNotNull(tetromino);
+        assertEquals(GameParameters.SPAWN_POINT, tetromino.getPosition());
+        assertNotSame(GameParameters.SPAWN_POINT, tetromino.getPosition());
+        assertNotSame(tetromino.getStateCord(), TetrominoType.valueOf(tetromino.getTypeValue()).getTetrominoState(0));
+        assertNotNull(tetromino.getColor());
+        assertEquals(0, tetromino.getCurrentState());
+        assertNotNull(tetromino.getStateCord());
+
     }
 
-    @Test
-    void tetrominoFactory() {
+    @ParameterizedTest
+
+    @EnumSource(value = TetrominoType.class, names = { "I", "O", "S", "Z", "L",
+            "J", "T" })
+    void tryDefineNewTetromino(TetrominoType names) {
+        // given
+        Tetromino tetromino = Tetromino.spawnSpecificTetromino(names, GameParameters.SPAWN_POINT);
+        // then
+        assertNotNull(tetromino);
+        assertEquals(GameParameters.SPAWN_POINT, tetromino.getPosition());
+        assertNotSame(GameParameters.SPAWN_POINT, tetromino.getPosition());
+        assertNotSame(tetromino.getStateCord(),
+                org.sehes.tetris.model.TetrominoType.valueOf(tetromino.getTypeValue()).getTetrominoState(0));
+        assertNotNull(tetromino.getColor());
+        assertEquals(0, tetromino.getCurrentState());
+        assertNotNull(tetromino.getStateCord());
     }
 
-    @Test
-    void getColor() {
-    }
-
-    @Test
-    void getStateCord() {
-    }
-
-    @Test
-    void getPosition() {
-    }
-
-    @Test
-    void move() {
-    }
-
-    @Test
-    void rotate() {
-    }
-
-    @Test
-    void setState() {
-    }
-
-    @Test
-    void spawnSpecficTetromino() {
-    }
-
-    @Test
-    void getCurrentState() {
-    }
 }
