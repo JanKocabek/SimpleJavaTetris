41302c72f8e004f478f16a20615e0b006098820d
Jan Kocábek <166728580+JanKocabek@users.noreply.github.com>
2026-03-11 08:42:43 +0100

    cleaning javadoc

--- src/main/java/org/sehes/tetris/Main.java
+++ src/main/java/org/sehes/tetris/Main.java
@@ -6,8 +6,7 @@ import org.sehes.tetris.controller.GameManager;
  * The Main class is the entry point of the Tetris game application. It is
  * responsible for initializing the game by creating a new instance of the
  * GameManager class.<br>
- * Game start from {@code GameManager} constructor.
- * 
+ * The game starts from the {@code GameManager} constructor. * 
  * @author Sehes
  * @version 0.5
  */
