b9a716a130b3d4c04fc9b61bea1a16782e3f03cc
Jan Kocábek <166728580+JanKocabek@users.noreply.github.com>
2026-03-12 19:10:37 +0100

    cleaning

--- src/test/java/org/sehes/tetris/model/TetrominoTest.java
+++ src/test/java/org/sehes/tetris/model/TetrominoTest.java
@@ -5,8 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
 import org.sehes.tetris.config.GameParameters;
 
 class TetrominoTest {
